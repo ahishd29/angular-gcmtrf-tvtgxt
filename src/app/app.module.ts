e7c655c24a8dfcdf8f119e55e80bf36de627cfcc
ahishd29 <ahishd29@gmail.com>
2021-04-16 13:38:36 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
@@ -8,17 +9,18 @@ import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
 import { ProductAlertsComponent } from "./product-alerts/product-alerts.component";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
-import { CartService } from './cart.service';
-import { CartComponent } from './cart/cart.component';
+import { CartService } from "./cart.service";
+import { CartComponent } from "./cart/cart.component";
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
-      { path : "cart", component : CartComponent },
+      { path: "cart", component: CartComponent }
     ])
   ],
   declarations: [
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 @Injectable()
@@ -17,5 +18,11 @@ export class CartService {
     return this.items;
   }
 
-  constructor() {}
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      "/assets/shipping.json"
+    );
+  }
+
+  constructor(private http: HttpClient) {}
 }
